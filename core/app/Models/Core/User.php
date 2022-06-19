d6d561a2d0628f14672b5b2471b1bf7b3e4c52ad
mortazavi <s.mohammad027@live.com>
2022-06-19 14:45:25 +0430

    models updated;

--- core/app/Models/Core/User.php
+++ core/app/Models/Core/User.php
@@ -2,34 +2,39 @@
 
     namespace App\Models\Core;
 
+    use App\Http\Resources\Core\User\UserCollection;
+    use App\Http\Resources\Core\User\UserResource;
     use App\Mail\ResetPasswordEmail;
+    use App\Models\Contracts\Filtering\Filterable;
+    use App\Models\Contracts\Filtering\Loadable;
+    use App\Models\Contracts\ResourceCollectionable;
+    use App\Models\Traits\Paginatable;
     use Hans\Alicia\Traits\AliciaRelationHandler;
     use Hans\Horus\HasRoles;
     use Hans\Horus\Models\Traits\HasRelations;
-    use Hans\Kollector\Traits\KollectorTrait;
     use Hans\Sphinx\Traits\SphinxTrait;
     use Illuminate\Auth\Notifications\VerifyEmail;
     use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
     use Illuminate\Contracts\Auth\MustVerifyEmail;
     use Illuminate\Database\Eloquent\Factories\HasFactory;
     use Illuminate\Foundation\Auth\User as Authenticatable;
+    use Illuminate\Http\Resources\Json\JsonResource;
+    use Illuminate\Http\Resources\Json\ResourceCollection;
     use Illuminate\Notifications\Notifiable;
     use Spatie\Activitylog\LogOptions;
     use Spatie\Activitylog\Traits\LogsActivity;
 
     /**
- * @mixin IdeHelperUser
- */
-    class User extends Authenticatable implements MustVerifyEmail, AuthenticatableContract {
+     * @mixin IdeHelperUser
+     */
+    class User extends Authenticatable implements MustVerifyEmail, AuthenticatableContract, Filterable, Loadable, ResourceCollectionable {
         use HasFactory, Notifiable, LogsActivity;
         use AliciaRelationHandler, HasRoles, HasRelations;
 
         use SphinxTrait, SphinxTrait {
             SphinxTrait::booted as private handleCaching;
         }
-        use KollectorTrait, KollectorTrait {
-            KollectorTrait::booted as private handleWallet;
-        }
+        use Paginatable;
 
         /**
          * The attributes that are mass assignable.
@@ -66,7 +71,6 @@
 
         protected static function booted() {
             self::handleCaching();
-            self::handleWallet();
         }
 
         public function getActivitylogOptions(): LogOptions {
@@ -104,11 +108,35 @@
 
         public function extract(): array {
             return [
-                'name' => $this->name
+                'name' => $this->name,
             ];
         }
 
         public static function username(): string {
             return 'email';
+        }
+
+        public function getFilterableAttributes(): array {
+            return [
+                'id',
+                'name',
+                'email',
+            ];
+        }
+
+        public function getLoadableRelations(): array {
+            return [];
+        }
+
+        public function getResource(): JsonResource {
+            return UserResource::make( ...func_get_args() );
+        }
+
+        public function toResource(): JsonResource {
+            return UserResource::make( $this, ... func_get_args() );
+        }
+
+        public function getResourceCollection(): ResourceCollection {
+            return UserCollection::make( ...func_get_args() );
         }
     }
